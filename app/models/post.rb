c670cacd3353a8a156b5d6483df6c15b61abd80b
Brian <bpalmer710@gmail.com>
2022-04-19 18:57:40 -0400

    commit tree 21257344d1cf98850151f36c26c1db81ff7d9c09

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,16 @@ class Post < ActiveRecord::Base
   has_many :categories, through: :post_categories
   has_many :comments
   has_many :users, through: :comments
+  accepts_nested_attributes_for :user
+
+  def categories_attributes=(category_attributes)
+    category_attributes.values.each do |category_attributes|
+      if category_attributes["name"].preset?
+        category = Category.find_or_create_by(category_attributes)
+        self.categories << category
+      end
+    end
+  end
 
 
 end
